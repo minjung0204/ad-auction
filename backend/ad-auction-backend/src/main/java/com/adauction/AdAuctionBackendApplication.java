c0dad0fbe48b61749bc2372da6e9b188273dcd99
Minjung Kim <fgh2018@naver.com>
2025-06-12 14:48:11 +0900

    feat: 백엔드 프로젝트 초기 설정 및 DDL 스크립트 추가

---
+++ backend/ad-auction-backend/src/main/java/com/adauction/AdAuctionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdAuctionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdAuctionBackendApplication.class, args);
+	}
+
+}
---
+++ backend/ad-auction-backend/src/test/java/com/adauction/AdAuctionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdAuctionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
